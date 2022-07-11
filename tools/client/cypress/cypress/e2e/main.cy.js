e9a056dd3c5c99395799562c9444ae393bba6e88
deanchen <antlet2004@gmail.com>
2022-07-11 10:26:45 +0800

    feat: install keycloak client &

--- tools/client/cypress/cypress/e2e/main.cy.js
+++ tools/client/cypress/cypress/e2e/main.cy.js
@@ -1,6 +1,7 @@
 
 describe('main', () => {
-  it('main', () => {
+  it('main workflow: login, request api', () => {
+    // login
     cy.visit('http://localhost:5000/');
     cy.contains("Client App");
     cy.contains("Sign in");
@@ -10,5 +11,9 @@ describe('main', () => {
     cy.contains("loaded token:");
     cy.contains("loaded user: dadior");
     cy.contains("loaded rpt:", { timeout: 1000 *20 });
+    
+    console.warn("request api");
+
+    cy.contains("API:settings").click();
   })
 })
